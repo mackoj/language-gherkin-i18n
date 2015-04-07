36c655e9bc58c97b79a6b7d71f724bdcc148084e
Jeffrey Macko <jeffrey@macko.fr>
2015-04-08 00:55:37 +0200

    The script seems working

--- updateFromi18n.php
+++ updateFromi18n.php
@@ -2,20 +2,20 @@
 
 $langKey = "__LANG__";
 $templateKeys = [ 
-	"__FEATURE__" => [ "name" => "feature", "separator" => "\\:|" ],
-	"__BACKGROUND__" => "background",
-	"__SCENARIO__" => "scenario",
-	"__SCENARIO__OUTLINE__" => "scenario_outline",
-	"__EXAMPLES__" => "examples",
-	"__GIVEN__" => "given",
-	"__WHEN__" => "when",
-	"__THEN__" => "then",
-	"__BUT__" => "but",
-	"__AND__" => "and",
+	"__FEATURE__" => [ "name" => "feature", "separator" => "\\\\:|" ],
+	"__BACKGROUND__" => [ "name" => "background", "separator" => "\\\\:|" ],
+	"__SCENARIO__" => [ "name" => "scenario", "separator" => "\\\\:|" ],
+	"__SCENARIO__OUTLINE__" => [ "name" => "scenario_outline", "separator" => "\\\\:|" ],
+	"__EXAMPLES__" => [ "name" => "examples", "separator" => "\\\\:|" ],
+	"__GIVEN__" => [ "name" => "given", "separator" => "|" ],
+	"__WHEN__" => [ "name" => "when", "separator" => "|" ],
+	"__THEN__" => [ "name" => "then", "separator" => "|" ],
+	"__BUT__" => [ "name" => "but", "separator" => "|" ],
+	"__AND__" => [ "name" => "and", "separator" => "|" ],
 ];
 $gherkinTemplate = dirname(__FILE__) . "/grammars/gherkin_template.cson";
 
-$i18nRemoteFilePath = "https://github.com/cucumber/gherkin/blob/master/lib/gherkin/i18n.json";
+$i18nRemoteFilePath = "https://raw.githubusercontent.com/cucumber/gherkin/master/lib/gherkin/i18n.json";
 // $cleaner = "^\*(.*)<?$";
 $delimiter = "|";
 $search1 = "*|";
@@ -36,18 +36,16 @@ $gherkinPathInfo = pathinfo($gherkinTemplate);
 foreach ($jsoni18nAssocArray as $jsonKey => $jsonValue) 
 {
 	$tmp_template = $base_template;
-	$tmp_template = $futureTemplate[$jsonKey];
 	$tmp_template = str_replace($langKey, $jsonKey, $tmp_template);
+		
 	foreach ($templateKeys as $tKey => $tValue) 
 	{
-		$futureRegex = $jsonValue[$tValue];
-		$futureRegex = str_replace($futureRegex, $search1);
-		$futureRegex = str_replace($futureRegex, $search2);
+		$futureRegex = $jsonValue[$tValue["name"]];
+		$futureRegex = str_replace($search1, "", $futureRegex);
+		$futureRegex = str_replace($search2, "", $futureRegex);
 		$explodedArray = explode($delimiter, $futureRegex);
-		foreach ($explodedArray as $explodedValue) 
-		{
-			$tmp_template = str_replace($tKey, $explodedValue, $tmp_template);
-		}
+		$tmp_str = implode($tValue["separator"], $explodedArray);
+		$tmp_template = str_replace($tKey, $tmp_str, $tmp_template);
 	}
 	$futureTemplate[$jsonKey] = $tmp_template;
 }
